b3b45a563aad833c8baefd4c205be99e28b84377
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-10-10 19:52:35 -0300

    "Criada a funcao seleciona questoes."
    Signed-off-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>

--- main.pl
+++ main.pl
@@ -3,8 +3,23 @@ imprimeQuestao([X|Y]):-
         format("~w\n", X),
         imprimeQuestao(Y).
 
+
+seleciona([X|_],0,H):-
+        imprimeQuestao(X),!.
+         
+seleciona([_|Z],Y,H):-
+        Y > 0, 
+        Y1 is Y-1,
+        seleciona(Z,Y1,H). 
+
 end() :-
 		write(" Parabéns, você me venceu!! Até a próxima.\n"),
 		halt(0).
 
-fase():-imprimeQuestao([['1'],'QUEM','1.michael jacson','2.bbbbbbbbb']),VERIFICAend().
+fase():- seleciona([
+    ['1- Quem inventou a lampada?','A) Thomas Edson.','B) João kleber','C) Ronaldinho Gaúcho.','D) Matheus Estanislau.'],
+    ['Qual é o maior país do mundo?','A) Butão.','B) Russia.','C) Venezuela.','D) Matheus Estanislau.'],['A'],['B']
+],1,A),seleciona([['1- Quem inventou a lampada?','A) Thomas Edson.','B) João kleber','C) Ronaldinho Gaúcho.','D) Matheus Estanislau.'],
+    ['Qual é o maior país do mundo?','A) Butão.','B) Russia.','C) Venezuela.','D) Matheus Estanislau.'],['A'],['B']],1+2,B),end().
+
+
