c7d6484cf8337b50b297d43e3080d6f74a4b16c2
Lucas Alexandre <lucas.alexandre.df@gmail.com>
2019-10-12 18:33:56 -0300

    Implementada a primeira fase
    Signed-off-by: Lucas Alexandre <lucas.alexandre.df@gmail.com>
    Co-authored-by: Matheu de Cristo Doreia Estanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: Renan Cristyan Araújo Pinheiro <rcristyan9@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- main.pl
+++ main.pl
@@ -4,22 +4,50 @@ imprimeQuestao([X|Y]):-
         imprimeQuestao(Y).
 
 
-seleciona([X|_],0,H):-
-        imprimeQuestao(X),!.
-         
-seleciona([_|Z],Y,H):-
-        Y > 0, 
-        Y1 is Y-1,
-        seleciona(Z,Y1,H). 
-
+        
 end() :-
 		write(" Parabéns, você me venceu!! Até a próxima.\n"),
 		halt(0).
 
-fase():- seleciona([
-    ['1- Quem inventou a lampada?','A) Thomas Edson.','B) João kleber','C) Ronaldinho Gaúcho.','D) Matheus Estanislau.'],
-    ['Qual é o maior país do mundo?','A) Butão.','B) Russia.','C) Venezuela.','D) Matheus Estanislau.'],['A'],['B']
-],1,A),seleciona([['1- Quem inventou a lampada?','A) Thomas Edson.','B) João kleber','C) Ronaldinho Gaúcho.','D) Matheus Estanislau.'],
-    ['Qual é o maior país do mundo?','A) Butão.','B) Russia.','C) Venezuela.','D) Matheus Estanislau.'],['A'],['B']],1+2,B),end().
+cls :- write('\33\[2J').
 
+perdeuJogo():-write("Deseja jogar novamente?\n s para sim\n n para nao\n"),get_char(s),fase(1).
+perdeuJogo():-write("NÃO CONSEGUE NÉ MOISES? ATÉ A PROXIMA\n"),halt(0).
+
+fase(1):-write("MAHH OLHA SÓ, VAI COMEÇAR O SHOW DO MILHÃO, VALENDO 1000 REAIS! (tananananananan)\n"),random(1,4,X), questao(X).
+ 
+questao(1):-cls,imprimeQuestao(['Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+'a) Tem entre 2 a 4 litros. São retirados 450 mililitros',
+'b) Tem entre 4 a 6 litros. São retirados 450 mililitros',
+'c) Tem 10 litros. São retirados 2 litros',
+'d) Tem 7 litros. São retirados 1,5 litros',
+'e) Tem 0,5 litros. São retirados 0,5 litros']), get_char(b), fase(2).
+questao(1):-write("Voce perdeu!!!\n"),sleep(1), perdeuJogo().
+
+questao(2):-cls,imprimeQuestao(['Qual o nome do presidente do Brasil que ficou conhecido como Jango”?',
+'a) Jânio Quadros',
+'b) Jacinto Anjos',
+'c) Getúlio Vargas',
+'d) João Figueiredo',
+'e) João Goulart']), get_char(e), fase(2).
+questao(2):-write("Voce perdeu!!!\n"), perdeuJogo().
+
+questao(3):-cls,imprimeQuestao(['Quais o menor e o maior país do mundo?',
+'a) Vaticano e Rússia',
+'b) Nauru e China',
+'c) Mônaco e Canadá',
+'d) Malta e Estados Unidos',
+'e) São Marino e Índia']), get_char(a), fase(2).
+questao(3):-write("Voce perdeu!!!\n"), perdeuJogo().
+
+questao(4):-cls,imprimeQuestao(['De onde é a invenção do chuveiro elétrico?',
+'a) França',
+'b) Inglaterra',
+'c) Brasil',
+'d) Austrália',
+'e) Itália']), get_char(c), fase(2).
+questao(4):-write("Voce perdeu!!!\n"), perdeuJogo().
+
+
+fase(2):-write("CERTA RESPOSTA (tanananan)\n"),write("MAHH OLHA SÓ\n VALENDO 10.000 REAIS!\n"),random(6,10,X), questao(X).
 
